864cd4a7354c61d107857e8e9957cf85a6e6a239
Sebastian Urbano <sebastian21.pernalete@gmail.com>
2021-04-09 23:06:23 -0400

    issue g in url

--- services/getShorterLink.js
+++ services/getShorterLink.js
@@ -1,12 +1,12 @@
-const API_URL = 'https://api.shrtco.de/v2'
+const API_URL = "https://api.shrtco.de/v2";
 
-export default async function getShorterLink (url) {
+export default async function getShorterLink(url) {
   try {
-    const data = await fetch(`${API_URL}/shorten?url=g${url}`)
-    const link = await data.json()
-    return link.result
+    const data = await fetch(`${API_URL}/shorten?url=${url}`);
+    const link = await data.json();
+    return link.result;
   } catch (err) {
-    console.error(err)
-    return err.response
+    console.error(err);
+    return err.response;
   }
 }
