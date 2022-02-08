16b259200c243e28c5a2335f83355ab50eff1fbe
Sebastián Urbano <42834763+sebastianupr@users.noreply.github.com>
2022-02-08 11:44:46 -0600

    Refactor of ShorterLinks managment and fetching
    * Fetching new ShorterLink with swr
    
    * Managment of ShorterLinks with Formik
    
    * Added ListButton, FormButton and adjustment of Button
    
    * Added SWR, Formik, yup and qs dependencies
    
    * Added Spinner
    
    * Added link storage services

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,11 +1,35 @@
-import style from './style.module.css'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-export default function Button ({ children, size = 'big' }) {
+import styles from './styles.module.css'
+
+export default function Button ({
+  children,
+  size = 'big',
+  type = 'button',
+  className,
+  ...props
+}) {
   return (
-    <div className={style.button}>
-      <button className={ size === 'big' ? style.buttonBig : style.buttonSmall}>
+    <div className={styles.buttonContainer}>
+      <button
+        className={classNames(
+          styles.button,
+          size === 'big' ? styles.buttonBig : styles.buttonSmall,
+          className
+        )}
+        type={type}
+        {...props}
+      >
         {children}
       </button>
     </div>
   )
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  size: PropTypes.oneOf(['big', 'small']),
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  className: PropTypes.string
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
+import { SWRConfig } from 'swr'
 import { Provider } from 'Context'
+
+import swrConfig from 'config/swr'
 
 import 'normalize.css/normalize.css'
 
@@ -8,15 +11,20 @@ import '../styles/Normal.css'
 
 export default function App ({ Component, pageProps }) {
   return (
-    <Provider>
-      <Head>
-        <title>Short links App - Sebastianupr</title>
+    <SWRConfig value={swrConfig}>
+      <Provider>
+        <Head>
+          <title>Short links App - Sebastianupr</title>
 
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet"></link>
-      </Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
 
-      <Component {...pageProps} />
-    </Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </SWRConfig>
   )
 }
